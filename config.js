3a6d4e19a5c9567eff370e8756c4a111311e31e1
JORGELBURGOS <jorgeleonburgos@gmail.com>
2026-07-06 13:29:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Config Sudameris Seguros — Apps Script Web App
 window.CONFIG = {
-  ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbzMkmGGvF6glooVb5L2mj_sArem5CbE9NjYlwj0ZrlKvLJgokEPdRRp5jNr3WOAqMYU/exec"
+  ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbxDp90brx4gvQ7QEW-Lfv2bg774yVY1lMDliqC7rS9K0oyNu00sd8MWI-8XPvkRksPk/exec"
 };
