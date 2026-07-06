7cc540503a68c5587f5932611c1fd67661197a7d
JORGELBURGOS <jorgeleonburgos@gmail.com>
2026-07-06 12:50:43 -0300

    Add configuration for Sudameris Seguros Web App

---
+++ config.js
@@ -0,0 +1,4 @@
+// Config Sudameris Seguros — Apps Script Web App
+window.CONFIG = {
+  ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbzMkmGGvF6glooVb5L2mj_sArem5CbE9NjYlwj0ZrlKvLJgokEPdRRp5jNr3WOAqMYU/exec"
+};
